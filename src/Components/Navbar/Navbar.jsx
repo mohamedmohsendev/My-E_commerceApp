eb5c10cac23457f690ef338001e451a1792eb9a6
Mohamed_Mohsen <mohsenmido75@gmail.com>
2025-09-20 11:02:07 +0300

    Update social media links and developer name - Updated Facebook, LinkedIn, and GitHub links to Mohamed Mohsen's profiles - Changed developer name from Ziad Faheem to Mohamed Mohsen - Improved code formatting and structure

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "./../../assets/images/freshcart-logo.svg";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { Usercontext } from "../../context/UserContext";
-import { productToCart } from './../../context/AddProductToCartContext';
+import { productToCart } from "./../../context/AddProductToCartContext";
 
 export default function Navbar() {
   const [visib, setvisib] = useState(false);
@@ -20,14 +20,11 @@ export default function Navbar() {
   return (
     <nav className="z-50 bg-white dark:bg-gray-900 fixed w-full top-0 left-0 right-0 shadow-md">
       <div className="md:w-[90%] mx-auto py-4 flex justify-between items-center flex-wrap">
-
-        
         <div className="flex justify-between items-center w-full md:w-auto">
           <NavLink to="/" className="text-3xl">
             <img src={logo} alt="logo" className="md:w-auto h-12" />
           </NavLink>
 
-         
           <i
             onClick={() => setvisib(!visib)}
             className="fa-solid text-3xl cursor-pointer md:hidden"
@@ -37,31 +34,38 @@ export default function Navbar() {
         </div>
 
         <div
-          className={`${visib ? "block" : "hidden"} md:flex justify-center items-center w-full md:w-auto mt-4 md:mt-0`}
+          className={`${
+            visib ? "block" : "hidden"
+          } md:flex justify-center items-center w-full md:w-auto mt-4 md:mt-0`}
         >
           {usertoken !== null ? (
             <ul className="flex flex-col md:flex-row gap-4 md:gap-8">
-           
               <div className="flex items-center gap-4">
                 <li className="text-center relative">
                   <NavLink
                     to="Woshlistcom"
-                    className={({ isActive }) => 
-                      isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "flex items-center space-x-2 text-green-500"
+                        : "flex items-center space-x-2"
                     }
                   >
-                    <i className="fa-solid fa-heart text-xl"></i> <span>Wishlist</span>
+                    <i className="fa-solid fa-heart text-xl"></i>{" "}
+                    <span>Wishlist</span>
                   </NavLink>
                 </li>
 
                 <li className="text-center relative">
                   <NavLink
                     to="Cart"
-                    className={({ isActive }) => 
-                      isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "flex items-center space-x-2 text-green-500"
+                        : "flex items-center space-x-2"
                     }
                   >
-                    <i className="fa-solid fa-cart-shopping text-xl"></i> <span>Cart</span>
+                    <i className="fa-solid fa-cart-shopping text-xl"></i>{" "}
+                    <span>Cart</span>
                     <div className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-green-500 rounded-full md:-top-3 -top-1 md:-right-3 right-34">
                       {numofitems === 0 && products?.length !== 0 ? (
                         <i className="fa-solid fa-spinner fa-spin"></i>
@@ -73,66 +77,92 @@ export default function Navbar() {
                 </li>
               </div>
 
-              
-              <div className="border-l-2 border-gray-400 h-6"></div> 
+              <div className="border-l-2 border-gray-400 h-6"></div>
 
-           
               <li className="text-center">
                 <NavLink
                   to="Catigories"
-                  className={({ isActive }) => 
-                    isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex items-center space-x-2 text-green-500"
+                      : "flex items-center space-x-2"
                   }
                 >
-                  <i className="fa-solid fa-th-list text-xl"></i> <span>Categories</span>
+                  <i className="fa-solid fa-th-list text-xl"></i>{" "}
+                  <span>Categories</span>
                 </NavLink>
               </li>
               <li className="text-center">
                 <NavLink
                   to="Products"
-                  className={({ isActive }) => 
-                    isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex items-center space-x-2 text-green-500"
+                      : "flex items-center space-x-2"
                   }
                 >
-                  <i className="fa-solid fa-cogs text-xl"></i> <span>Products</span>
+                  <i className="fa-solid fa-cogs text-xl"></i>{" "}
+                  <span>Products</span>
                 </NavLink>
               </li>
               <li className="text-center">
                 <NavLink
                   to="Brands"
-                  className={({ isActive }) => 
-                    isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex items-center space-x-2 text-green-500"
+                      : "flex items-center space-x-2"
                   }
                 >
-                  <i className="fa-solid fa-tags text-xl"></i> <span>Brands</span>
+                  <i className="fa-solid fa-tags text-xl"></i>{" "}
+                  <span>Brands</span>
                 </NavLink>
               </li>
               <li className="text-center">
                 <NavLink
                   to="allorders"
-                  className={({ isActive }) => 
-                    isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex items-center space-x-2 text-green-500"
+                      : "flex items-center space-x-2"
                   }
                 >
-                  <i className="fa-solid fa-box text-xl"></i> <span>All Orders</span>
+                  <i className="fa-solid fa-box text-xl"></i>{" "}
+                  <span>All Orders</span>
                 </NavLink>
               </li>
             </ul>
           ) : null}
         </div>
 
-     
         <div
-          className={`${visib ? "block" : "hidden"} md:flex items-center gap-4 w-full md:w-auto mt-4 md:mt-0`}
+          className={`${
+            visib ? "block" : "hidden"
+          } md:flex items-center gap-4 w-full md:w-auto mt-4 md:mt-0`}
         >
           <div className="icons text-center flex gap-6">
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-black transition-all duration-200">
+            <a
+              href="https://www.linkedin.com/in/mohamed-mohsen-9865b916b/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-800 hover:text-black transition-all duration-200"
+            >
               <i className="fa-brands fa-linkedin-in text-xl"></i>
             </a>
-            <a href="https://www.facebook.com/ziad20022" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-black transition-all duration-200">
+            <a
+              href="https://www.facebook.com/mohamedmohsenmadbouly"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-800 hover:text-black transition-all duration-200"
+            >
               <i className="fa-brands fa-facebook-f text-xl"></i>
             </a>
-            <a href="https://github.com/ziad845" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-black transition-all duration-200">
+            <a
+              href="https://github.com/mohamedmohsendev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-800 hover:text-black transition-all duration-200"
+            >
               <i className="fa-brands fa-github text-xl"></i>
             </a>
           </div>
@@ -143,27 +173,37 @@ export default function Navbar() {
                 <li className="text-center">
                   <NavLink
                     to="Login"
-                    className={({ isActive }) => 
-                      isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "flex items-center space-x-2 text-green-500"
+                        : "flex items-center space-x-2"
                     }
                   >
-                    <i className="fa-solid fa-sign-in-alt text-xl"></i> <span>Login</span>
+                    <i className="fa-solid fa-sign-in-alt text-xl"></i>{" "}
+                    <span>Login</span>
                   </NavLink>
                 </li>
                 <li className="text-center">
                   <NavLink
                     to="Register"
-                    className={({ isActive }) => 
-                      isActive ? "flex items-center space-x-2 text-green-500" : "flex items-center space-x-2"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "flex items-center space-x-2 text-green-500"
+                        : "flex items-center space-x-2"
                     }
                   >
-                    <i className="fa-solid fa-user-plus text-xl"></i> <span>Register</span>
+                    <i className="fa-solid fa-user-plus text-xl"></i>{" "}
+                    <span>Register</span>
                   </NavLink>
                 </li>
               </>
             ) : (
-              <li className="text-center cursor-pointer flex items-center space-x-2 hover:text-green-500" onClick={() => logout()}>
-                <i className="fa-solid fa-sign-out-alt text-xl"></i> <span>Logout</span>
+              <li
+                className="text-center cursor-pointer flex items-center space-x-2 hover:text-green-500"
+                onClick={() => logout()}
+              >
+                <i className="fa-solid fa-sign-out-alt text-xl"></i>{" "}
+                <span>Logout</span>
               </li>
             )}
           </ul>
